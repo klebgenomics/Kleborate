7b3cfed3ba8d101df5c2b6ccea1a85eab305b278
Ryan Wick <rrwick@gmail.com>
2017-06-19 14:47:34 +1000

    Fix setup script

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='Kleborate',
       test_suite='nose.collector',
       tests_require=[],
       entry_points={
-          'console_scripts': ['kleborate=kleborate.kleborate:kleborate'],
+          'console_scripts': ['kleborate = kleborate.kleborate:main'],
       },
       include_package_data=True,
       zip_safe=False)
