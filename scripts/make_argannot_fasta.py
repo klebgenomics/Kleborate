e197cc08cf6aef8cb4302dbd99a9e6b755975deb
Ryan Wick <rrwick@gmail.com>
2017-10-31 14:00:44 +1100

    Fix Colistin class name in fasta

--- scripts/make_argannot_fasta.py
+++ scripts/make_argannot_fasta.py
@@ -29,6 +29,7 @@ def main():
 
     for name, seq in fasta_seqs:
         name = name.replace('NimB_Nitroimidazole_Gene', 'NimB')  # fix verbose gene name
+        name = name.replace('_Colistin__', '_Col__')  # colistin class name to just three letters
         allele_name = name.split('__')[2]
         if allele_name in allele_names:
             print('>' + name)
