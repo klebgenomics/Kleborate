5646ab73ded83e4cae3cee9f1d17e3f13d837bd9
Ryan Wick <rrwick@gmail.com>
2019-03-21 16:33:00 +1100

    Add some tests for planned amino acid matching

--- test/test_res_alleles.py
+++ test/test_res_alleles.py
@@ -26,8 +26,55 @@ class TestResAlleles(unittest.TestCase):
     """
 
     def setUp(self):
-        self.data_dir = get_data_path()
-
+        self.data_dir = 'test/res_test/data'
         Args = collections.namedtuple('Args', ['species', 'resistance', 'kaptive_k', 'kaptive_o'])
         self.args = Args(species=False, resistance=True, kaptive_k=False, kaptive_o=False)
         _, _, self.res_headers = get_output_headers(self.args, self.data_dir)
+
+    def test_res_01(self):
+        results = get_resistance_results(self.data_dir, 'test/res_test/01.fasta', self.args,
+                                         self.res_headers)
+        self.assertEqual(results['Flq'], 'ABC-1')
+        self.assertEqual(results['Tet'], '-')
+        self.assertEqual(results['Bla'], '-')
+        self.assertEqual(results['Bla_Carb'], '-')
+        self.assertEqual(results['Bla_ESBL'], '-')
+        self.assertEqual(results['Bla_broad'], '-')
+
+    def test_res_02(self):
+        results = get_resistance_results(self.data_dir, 'test/res_test/02.fasta', self.args,
+                                         self.res_headers)
+        self.assertEqual(results['Flq'], 'ABC-2')
+        self.assertEqual(results['Tet'], '-')
+        self.assertEqual(results['Bla'], '-')
+        self.assertEqual(results['Bla_Carb'], '-')
+        self.assertEqual(results['Bla_ESBL'], '-')
+        self.assertEqual(results['Bla_broad'], '-')
+
+    def test_res_03(self):
+        """
+        This query allele is equally distant from both ABC-1 and ABC-2 (two substitutions), but an
+        exact amino acid match for each. While both alleles are an acceptable answer, we should
+        return ABC-1 because it's the lower numbered allele.
+        """
+        results = get_resistance_results(self.data_dir, 'test/res_test/03.fasta', self.args,
+                                         self.res_headers)
+        self.assertEqual(results['Flq'], 'ABC-1*')
+        self.assertEqual(results['Tet'], '-')
+        self.assertEqual(results['Bla'], '-')
+        self.assertEqual(results['Bla_Carb'], '-')
+        self.assertEqual(results['Bla_ESBL'], '-')
+        self.assertEqual(results['Bla_broad'], '-')
+
+    def test_res_04(self):
+        """
+        This query allele has no perfect nucleotide matches but it matches ABC-3's amino acids.
+        """
+        results = get_resistance_results(self.data_dir, 'test/res_test/04.fasta', self.args,
+                                         self.res_headers)
+        self.assertEqual(results['Flq'], 'ABC-3^')
+        self.assertEqual(results['Tet'], '-')
+        self.assertEqual(results['Bla'], '-')
+        self.assertEqual(results['Bla_Carb'], '-')
+        self.assertEqual(results['Bla_ESBL'], '-')
+        self.assertEqual(results['Bla_broad'], '-')
