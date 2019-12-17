41308d900ce6ca29b773ab285001bdc753cdf913
Ryan Wick <rrwick@gmail.com>
2019-12-17 15:33:11 +1100

    New unit tests to cover tricky Aac genes

---
+++ test/test_res_aac.py
@@ -0,0 +1,55 @@
+"""
+Copyright 2018 Kat Holt
+Copyright 2018 Ryan Wick (rrwick@gmail.com)
+https://github.com/katholt/Kleborate/
+
+This file is part of Kleborate. Kleborate is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version. Kleborate is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details. You should have received a copy of the GNU General Public License along with Kleborate. If
+not, see <http://www.gnu.org/licenses/>.
+"""
+
+import collections
+import unittest
+
+from kleborate.kleborate import get_output_headers, get_resistance_results
+
+
+class TestResAlleles(unittest.TestCase):
+    """
+    Tests calling of resistance via alleles.
+    """
+
+    def setUp(self):
+        self.data_dir = 'test/aac_test/data'
+        Args = collections.namedtuple('Args', ['resistance', 'kaptive_k', 'kaptive_o'])
+        self.args = Args(resistance=True, kaptive_k=False, kaptive_o=False)
+        _, _, self.res_headers = get_output_headers(self.args, self.data_dir)
+
+    def test_res_01(self):
+        results = get_resistance_results(self.data_dir, 'test/aac_test/01.fasta', self.args,
+                                         self.res_headers, True)
+        self.assertEqual(results['AGly'], '-')
+
+    def test_res_02(self):
+        results = get_resistance_results(self.data_dir, 'test/aac_test/02.fasta', self.args,
+                                         self.res_headers, True)
+        self.assertEqual(results['AGly'], 'Aac6-31')
+
+    def test_res_03(self):
+        results = get_resistance_results(self.data_dir, 'test/aac_test/03.fasta', self.args,
+                                         self.res_headers, True)
+        self.assertEqual(results['AGly'], 'Aac6-31*')
+
+    def test_res_04(self):
+        """
+        This is a tricky one (and the one which first highlighted the problem). The best bit-score
+        match is for a partial hit to Ant3''Ih-Aac6-IId, but the correct answer is Aac6Ib-cr, which
+        has a full coverage hit.
+        """
+        results = get_resistance_results(self.data_dir, 'test/aac_test/04.fasta', self.args,
+                                         self.res_headers, True)
+        self.assertEqual(results['AGly'], 'Aac6Ib-cr*')
