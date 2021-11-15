a3dd7e7364a0b17065586a4a30a3e7fc8b837977
Ryan Wick <rrwick@gmail.com>
2021-11-15 12:51:58 +1100

    Get K and O types from Kaptive

--- test/test_kaptive.py
+++ test/test_kaptive.py
@@ -14,58 +14,10 @@ not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 
-from kleborate.kaptive import get_o_type, confidence_meets_threshold
+from kleborate.kaptive import confidence_meets_threshold
 
 
 class TestKaptive(unittest.TestCase):
-
-    def test_o_type_01(self):
-        self.assertEqual(get_o_type('O1v1'), 'O1')
-
-    def test_o_type_02(self):
-        self.assertEqual(get_o_type('O1v2'), 'O1')
-
-    def test_o_type_03(self):
-        self.assertEqual(get_o_type('O2v1'), 'O2')
-
-    def test_o_type_04(self):
-        self.assertEqual(get_o_type('O2v2'), 'O2')
-
-    def test_o_type_05(self):
-        self.assertEqual(get_o_type('O1/O2v1'), 'unknown')
-
-    def test_o_type_06(self):
-        self.assertEqual(get_o_type('O1/O2v2'), 'unknown')
-
-    def test_o_type_07(self):
-        self.assertEqual(get_o_type('O3/O3a'), 'O3/O3a')
-
-    def test_o_type_08(self):
-        self.assertEqual(get_o_type('O3b'), 'O3b')
-
-    def test_o_type_09(self):
-        self.assertEqual(get_o_type('O4'), 'O4')
-
-    def test_o_type_10(self):
-        self.assertEqual(get_o_type('O5'), 'O5')
-
-    def test_o_type_11(self):
-        self.assertEqual(get_o_type('O8'), 'O8')
-
-    def test_o_type_12(self):
-        self.assertEqual(get_o_type('O12'), 'O12')
-
-    def test_o_type_13(self):
-        self.assertEqual(get_o_type('OL101'), 'OL101')
-
-    def test_o_type_14(self):
-        self.assertEqual(get_o_type('OL102'), 'OL102')
-
-    def test_o_type_15(self):
-        self.assertEqual(get_o_type('OL103'), 'OL103')
-
-    def test_o_type_16(self):
-        self.assertEqual(get_o_type('OL104'), 'OL104')
 
     def test_confidence_meets_threshold_01(self):
         self.assertTrue(confidence_meets_threshold('None', 'None'))
