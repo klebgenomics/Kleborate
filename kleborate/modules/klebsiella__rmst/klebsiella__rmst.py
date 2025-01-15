0210a2491139f516ca6fcc55c7d6806530dc04e7
Mary Maranga <38289520+Marysteph@users.noreply.github.com>
2025-01-15 08:11:01 +0000

    Update klebsiella__rmst.py

--- kleborate/modules/klebsiella__rmst/klebsiella__rmst.py
+++ kleborate/modules/klebsiella__rmst/klebsiella__rmst.py
@@ -1,6 +1,5 @@
 """
-Copyright 2023 Kat Holt
-Copyright 2023 Ryan Wick (rrwick@gmail.com)
+Copyright 2025 Kat Holt, Mary Maranga, Ryan Wick
 https://github.com/katholt/Kleborate/
 
 This file is part of Kleborate. Kleborate is free software: you can redistribute it and/or modify
@@ -45,8 +44,10 @@ def add_cli_options(parser):
                        help='Minimum alignment percent identity for klebsiella__rmst spurious results')
     group.add_argument('--klebsiella__rmst_min_spurious_coverage', type=float, default=40.0,
                        help='Minimum alignment percent coverage for klebsiella__rmst spurious results')
-    group.add_argument('--klebsiella__rmst_required_exact_matches', type=int, default=2,
+    group.add_argument('--klebsiella__rmst_required_exact_matches', type=int, default=1,
                        help='At least this many exact matches are required to call an ST')
+    group.add_argument('--klebsiella__rmst_min_gene_count', type=int, default=2,
+                       help='At least this many exact alleles required to report an "unknown" group')
     return group
 
 
@@ -59,8 +60,8 @@ def check_cli_options(args):
         sys.exit('Error: --klebsiella__rmst_min_spurious_identity must be between 50.0 and 100.0')
     if args.klebsiella__rmst_min_spurious_coverage <= 30.0 or args.klebsiella__rmst_min_spurious_coverage >= 100.0:
         sys.exit('Error: --klebsiella__rmst_min_spurious_coverage must be between 30.0 and 100.0')
-    if args.klebsiella__rmst_required_exact_matches < 0:
-        sys.exit('Error: --klebsiella__rmst_required_exact_matches must be a positive integer')
+    if args.klebsiella__rmst_min_gene_count < 0:
+        sys.exit('Error: --klebsiella__rmst_min_gene_count must be a positive integer')
 
 
 def check_external_programs():
@@ -83,7 +84,9 @@ def get_results(assembly, minimap2_index, args, previous_results):
                                       args.klebsiella__rmst_min_coverage, args.klebsiella__rmst_required_exact_matches,
                                       check_for_truncation=True, report_incomplete=True,
                                       min_spurious_identity=args.klebsiella__rmst_min_spurious_identity,
-                                      min_spurious_coverage=args.klebsiella__rmst_min_spurious_coverage)
+                                      min_spurious_coverage=args.klebsiella__rmst_min_spurious_coverage,
+                                      unknown_group_name='rmp unknown',
+                                      min_gene_count=args.klebsiella__rmst_min_gene_count)
     st, lineage, alleles = results
 
     if st == 'NA':
